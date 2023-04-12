4a9c58d364d033041e6012e9751beccc2f9697b4
Mikhail <mehan@list.ru>
2023-04-12 20:59:38 +0300

    fileoperations implementation

--- src/model/FileOperationsImpl.java
+++ src/model/FileOperationsImpl.java
@@ -1,17 +1,60 @@
 package model;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileOperationsImpl implements FileOperations{
 
     private String fileName;
-    @Override
-    public void putAllToys(List<Toy> toys) {
 
+    public FileOperationsImpl(String fileName){
+        this.fileName = fileName;
+        try (FileWriter csvWriter = new FileWriter(fileName, true)){
+            csvWriter.flush();
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    @Override
+    public void putAllToys(List<String> toys) {
+        try (FileWriter csvWriter = new FileWriter(fileName, false)){
+            for (String toy: toys){
+                csvWriter.write(toy);
+                csvWriter.append("\n");
+            }
+            csvWriter.flush();
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
-    public List<Toy> getOutAllToys() {
-        return null;
+    public List<String> getAllToys() {
+        List<String> allToys = new ArrayList<>();
+        try{
+            File file = new File(fileName);
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = bufferedReader.readLine();
+            if (line != null){
+                allToys.add(line);
+            }
+            while (line != null){
+                line = bufferedReader.readLine();
+                if (line != null){
+                    allToys.add(line);
+                }
+            }
+            fileReader.close();
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return allToys;
     }
+
 }
---
+++ src/model/ToyStorage.java
@@ -0,0 +1,9 @@
+package model;
+
+import java.util.List;
+
+public interface ToyStorage {
+    List<Toy> getAllToys();
+
+
+}
